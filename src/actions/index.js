1b89650bf3b124f582bd1cd743798958ad8ea090
Ivan5646 <Mike000027@yandex.ru>
2019-01-24 00:37:40 +0700

    pagination started

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,10 @@ export const receiveTasks = (result) => ({
     result: result,
 });
 
-export function fetchTasks() {
+export function fetchTasks(pageNumber) {
     return function (dispatch) {
         dispatch(requestTasks());
-        return fetch("https://uxcandy.com/~shapoval/test-task-backend/?developer=mikhai")
+        return fetch(`https://uxcandy.com/~shapoval/test-task-backend/?developer=mikhai&page=${pageNumber}`)
             .then(
                 response => response.json(),
                 error => console.log('An error occurred.', error),
@@ -27,27 +27,48 @@ export function fetchTasks() {
 
 export const createTask = (task) => {
     return (dispatch) => {
-        var form = new FormData();
-        form.append("username", "333");
-        form.append("email", "333@ggg.com");
-        form.append("text", "333 text");
+        // var form = new FormData();
+        // form.append("username", "333");
+        // form.append("email", "333@ggg.com");
+        // form.append("text", "333 text");
 
-        jquery.ajax({
-            url: 'https://uxcandy.com/~shapoval/test-task-backend/create?developer=mikhai',
-            crossDomain: true,
-            method: 'POST',
-            mimeType: "multipart/form-data",
-            contentType: false,
-            processData: false,
-            data: form,
-            dataType: "json",
-            success: data => {
-                dispatch(createTaskSuccess(data));
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
+        const taskData = {username: "petro", email: "petroMail", text: "random text so random"};
+
+        function myFunction(data, callback) { // request is failing
+            var result = false;
+            jquery.ajax({
+                url: 'https://uxcandy.com/~shapoval/test-task-backend/create?developer=mikhai', // 'http://ptsv2.com/t/0o8hh-1548184815/post',
+                crossDomain: true,
+                method: 'POST',
+                mimeType: "multipart/form-data",
+                contentType: false,
+                processData: false,
+                data: data,
+                dataType: "jsonp",
+                success: data => {
+                    //dispatch(createTaskSuccess(data));
+                    console.log("post successful");
+                    result = data;
+                    if(callback != null) {
+                        callback(result);
+                    }
+                },
+                error: function (error) {
+                    console.log("post error", error);
+                    //result = false;
+                    result = data;
+                    if(callback != null)  {
+                        callback(result);
+                    }
+                }
+            });
+        }
+        function processData(result) {
+            console.log("result for dispatch", result);
+            dispatch(createTaskSuccess(result));
+        }
+        myFunction(taskData, processData);
+
     }
 };
 
--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import {bindActionCreators} from 'redux';
+import Pagination from "react-js-pagination";
+//require("bootstrap/less/bootstrap.less");
 import { fetchTasks } from "../actions/index";
+import AddTask from "./AddTask"
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: 1
+        };
+        this.handlePageChange = this.handlePageChange.bind(this);
+    }
+
     componentDidMount(){
-        this.props.fetchTasks()
+        this.props.fetchTasks();
+    }
+
+    // loadTasks() {
+    //     this.props.fetchTasks()
+    // }
+
+    handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        this.setState({activePage: pageNumber});
+        this.props.fetchTasks(pageNumber);
     }
 
     render() {
@@ -13,6 +34,7 @@ class TodoList extends React.Component {
             return (
                 <div>
                     <h3>Tasks</h3>
+                    <AddTask/>
                     <div>{
                         this.props.tasks.map((task) => {
                             return (
@@ -24,6 +46,13 @@ class TodoList extends React.Component {
                             )
                         })
                     }</div>
+                    <Pagination
+                        activePage={this.state.activePage}
+                        itemsCountPerPage={10}
+                        totalItemsCount={18}
+                        pageRangeDisplayed={5}
+                        onChange={this.handlePageChange}
+                    />
                 </div>
             )
         } else {
