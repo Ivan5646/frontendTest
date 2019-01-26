ac9c671239d087175c2c29e3d3c78333aced6cdf
Ivan5646 <Mike000027@yandex.ru>
2019-01-27 00:16:47 +0700

    persist loginReducer to LS

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import { logger } from 'redux-logger'
 import reducer from './reducers'
 //import './css/styles.css'
 import App from "./components/App"
+import { loadState, saveState } from "./persistState/localStorage";
 
-
+const persistedState = loadState();
 const store = createStore(
     reducer,
+    persistedState,
     applyMiddleware(thunk, logger)
 )
 
@@ -20,3 +22,9 @@ render(
     </Provider>,
     document.getElementById('root')
 )
+
+store.subscribe(() => {
+    saveState({
+        login: store.getState().login
+    });
+});
